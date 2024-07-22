00bb3ef72f0eb394a70b7bc1ec4de9235e5e2f8d
miraiy-outh <shokolatemeur@mail.ru>
2024-07-22 12:45:50 +0300

    add katakana & change styles

--- src/pages/alphabet/katakana/katakana.tsx
+++ src/pages/alphabet/katakana/katakana.tsx
@@ -1,9 +1,11 @@
+import { Cards } from "../../../components/cards/cards";
 import MainMenu from "../../../components/main-menu/main-menu";
 
 export function Katakana() {
   return (
     <>
       <MainMenu />
+      <Cards type="katakana" />
     </>
   );
 }
